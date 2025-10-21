58aad5517d564b8d7c2d2ecab0144cc27a896a80
yakupovdev <sperslava@gmail.com>
2025-10-21 23:22:26 +0500

    feat: add main

--- src/com/github/yakupovdev/javainterviewapp/Main.java
+++ src/com/github/yakupovdev/javainterviewapp/Main.java
@@ -1,7 +1,10 @@
 package com.github.yakupovdev.javainterviewapp;
 
+import com.github.yakupovdev.javainterviewapp.objects.ObjectInitializer;
+
 public class Main {
     public static void main(String[] args) {
-
+        ObjectInitializer objectInitializer = new ObjectInitializer();
+        objectInitializer.run();
     }
 }
